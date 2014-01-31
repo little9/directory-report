d3f6187b51ecd84d410e29c2825fb4ac082cbe26
James Little <j.little@miami.edu>
2014-01-31 13:05:11 -0500

    Initial

---
+++ directory-report.py
@@ -0,0 +1,34 @@
+#!/usr/bin/evn python
+
+# This script generates a report that provides information about files on the archive server
+
+from subprocess import Popen, PIPE
+import xml.etree.ElementTree as ET
+
+def gen_directory_report(path):
+    data = []
+    directory_usage = Popen(["du", "-h", path], stdout=PIPE).communicate()[0]
+    directory_usage_list = [s.strip().split('\t') for s in directory_usage.splitlines()]
+    for i in directory_usage_list:
+
+        cmd = "find '{0}' -type f | sed 's/.*\.//' | sort | uniq -c".format(i[1])
+
+        p = Popen(cmd, stdout=PIPE, stderr=PIPE, bufsize=256*1024*1024, shell=True)
+       
+        file_count, file_info = p.communicate()
+   
+        data.append(i[1] + "," + i[0] + "," + file_count.replace('\n',' ').replace('\t',''))
+    
+    return data 
+
+
+
+
+
+
+report_data = gen_directory_report('/Users/jlittle/Music')
+print "Directory Name, Directory Size, Directory File Type Count"
+
+for i in report_data:
+    i.replace('\n', ',')
+    print i
